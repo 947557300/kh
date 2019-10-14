6eabd760f5d566c493b36569feb47dbf8fdd8abb
krystal <947557300@qq.com>
2019-10-14 18:25:22 +0800

    1

---
+++ 仿糗事百科/components/index/tabtop.vue
@@ -0,0 +1,29 @@
+<template>
+				<view class="nav_top"  :style="{height:headHeight+'px'}">
+					<view class="statusBar" :style="{height:statusBarHeight+'px'}"></view>
+					<view>sdsdsdsd5454</view>
+				</view>
+</template>
+
+<script>
+	export default{
+		  data(){
+			  return{
+			  statusBarHeight:"",
+			  headHeight:"",
+		  }
+		  },
+		created(){
+	        const system = uni.getSystemInfoSync()
+	   		this.statusBarHeight = system.statusBarHeight || 25
+	   		this.headHeight = (system.statusBarHeight || 25) + 45//可调整
+		}
+	}
+</script>
+  
+
+<style>
+.nav_top{
+  background:red;	
+}	
+</style>
---
+++ 仿糗事百科/pages/news/news.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		dongtai
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
